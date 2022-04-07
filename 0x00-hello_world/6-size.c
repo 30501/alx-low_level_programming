e3f4547ebed83e8ca151fbbad05595064c94c87d
30501 <berryhunex@gmail.com>
2022-04-07 12:54:17 -0700

    my commit for task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 /**
+ * main - Entry point
  *
- *main - Entry point
- *
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-printf("size of a char: %d bytes(s)\n", sizeof(char));
-printf("size of an int: %d bytes(s)\n", sizeof(int));
-printf("size of long int: %d bytes(s)\n", sizeof(long int));
-printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
-printf("size of a float: %d bytes(s)\n", sizeof(float));
+int a;
+long int b;
+long long int c;
+char d;
+float f;
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
