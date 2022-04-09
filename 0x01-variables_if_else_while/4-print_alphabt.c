cdf5ed8192c6b2076d90dcefc7d69663f2421ddc
30501 <101524548+30501@users.noreply.github.com>
2022-04-10 00:06:14 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+  
+{
+ char alphabet;
+  
+  for (alpahbet = 'a' ; alphabet <= 'z' ; alphabet++)
+  {
+    if (alphabet == 'q')
+      continue;
+    
+    else if (alphbaet == 'e')
+      continue;
+    
+    putchar(alphabet);
+  }
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
