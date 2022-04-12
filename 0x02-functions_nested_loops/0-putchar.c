52bba2408e59cf32d03720575bd98cab2c75e25e
30501 <101524548+30501@users.noreply.github.com>
2022-04-12 23:32:08 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,14 +4,14 @@
  * @void: no argument
  * Return: 0 Always (success/correct)
  */
- int main(void)
- {
- char i[] = "_putchar";
- int c;
- for (c = 0; c < 8; c++)
- {
- _putchar(i[c]);
- }
- _putchar('\n');
- return (0);
- }
+int main(void)
+{
+char i[] = "_putchar";
+int c;
+for (c = 0; c < 8; c++)
+{
+_putchar(i[c]);
+}
+_putchar('\n');
+return (0);
+}
